e06c6da8ffa3a0f4aed7a4754de52e97d5c56f95
Trenth01 <96491309+Trenth01@users.noreply.github.com>
2024-11-19 14:38:10 +0000

    Day 2 - correcting gateway proxy and making app accessible from gloo gateway

--- flask_app/app.py
+++ flask_app/app.py
@@ -4,4 +4,4 @@ if __name__ == '__main__':
     app = init_app()
     current_host = app.config.get('HOST')
     current_port = app.config.get('PORT')
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
